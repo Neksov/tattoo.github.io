4ac06bcf1c3acc44ca059094488fd492af43a9b9
Neksov <n2v@list.ru>
2020-06-04 16:00:29 +0300

    add anime and rewrite for mobile view

--- js/main.js
+++ js/main.js
@@ -1,3 +1,27 @@
-$('.carousel').carousel({
-  interval: 1000
-})
+$(document).ready(function () {
+  //плавный якорь
+  $(function () {
+    $('a[href^="#"]').on("click", function (event) {
+      // отменяем стандартное действие
+      event.preventDefault();
+
+      var sc = $(this).attr("href"),
+        dn = $(sc).offset().top;
+      /*
+       * sc - в переменную заносим информацию о том, к какому блоку надо перейти
+       * dn - определяем положение блока на странице
+       */
+      $("html, body").animate({
+          scrollTop: dn
+        },
+        1000
+      );
+      /*
+       * 1000 скорость перехода в миллисекундах
+       */
+    });
+  });
+  new WOW().init();
+  $('.collapse').removeClass('nav-item');
+
+});
