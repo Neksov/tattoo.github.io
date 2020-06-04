fb8099d5b5c2a9d0c322a65d5492f38042b496c0
Neksov <n2v@list.ru>
2020-06-04 21:58:45 +0300

    rewrite block question

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
 $(document).ready(function () {
+  new WOW().init();
+
+  $('.collapse').removeClass('nav-item');
+
+  $(".nav-item").click(function (event) {
+    $(".navbar-collapse, .nav-item").removeClass("show");
+  });
+
   //плавный якорь
   $(function () {
     $('a[href^="#"]').on("click", function (event) {
@@ -11,17 +19,16 @@ $(document).ready(function () {
        * sc - в переменную заносим информацию о том, к какому блоку надо перейти
        * dn - определяем положение блока на странице
        */
+
       $("html, body").animate({
-          scrollTop: dn
+          scrollTop: dn,
         },
         1000
       );
+
       /*
        * 1000 скорость перехода в миллисекундах
        */
     });
   });
-  new WOW().init();
-  $('.collapse').removeClass('nav-item');
-
 });
